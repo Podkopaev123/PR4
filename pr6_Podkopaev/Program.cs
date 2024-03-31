07ea71d389ac8aeca4e3e0371c9f06798747d981
Podkapaev123 <Podkapaev1mok@mail.ru>
2024-03-31 18:21:22 +0300

    Вывод значения на экран

--- pr6_Podkopaev/Program.cs
+++ pr6_Podkopaev/Program.cs
@@ -14,6 +14,7 @@ namespace pr6_Podkopaev
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
             double F = 2 * Math.Cos(x);
+            Console.WriteLine($"Для значения х = { x}, F = { F}."ВЫ);
         }
     }
 }
